6509fbafb3b8c1d7cbcae11190aeee7ed59b2be1
amadousysada <amsada93@gmail.com>
2019-03-06 02:02:23 +0100

    2.2.1

---
+++ tp2-2.1.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from sklearn.dummy import DummyClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn import tree
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+from sklearn import metrics
+from scipy.io import arff
+from sklearn.impute import SimpleImputer
+
+dummycl = DummyClassifier(strategy="most_frequent")
+gmb = GaussianNB()
+dectree = tree.DecisionTreeClassifier()
+logreg = LogisticRegression(solver="liblinear")
+svc = svm.SVC(gamma='scale')
+
+data, meta = arff.loadarff("crx.arff")
+imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp_mean.fit_transform(data)
+lst_classif = [dummycl, gmb, dectree, logreg, svc]
+lst_classif_names = ['Dummy', 'Naive Bayes', 'Decision tree', 'Logistic regression', 'SVM']
+
+def accuracy_score(lst_classif,lst_classif_names,X,y):
+    for clf,name_clf in zip(lst_classif,lst_classif_names):
+        scores = cross_val_score(clf, X, y, cv=5)
+        print("Accuracy of "+name_clf+" classifier on cross-validation: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+def confusion_matrix(lst_classif,lst_classif_names,X,y):
+    for clf,name_clf in zip(lst_classif,lst_classif_names):
+        predicted = cross_val_predict(clf, X, y, cv=5) 
+        print("Accuracy of "+name_clf+" classifier on cross-validation: %0.2f" % metrics.accuracy_score(y, predicted))
+        print(metrics.confusion_matrix(y, predicted))
+
+
+
+
+        
+
+        
+# Replace missing values by mean and scale numeric values
+data_num = data.select_dtypes(include='float64')
+
+
+
+
+
+# Replace missing values by mean and discretize categorical values
+data_cat = data.select_dtypes(exclude='float64').drop('class',axis=1)
+
+
